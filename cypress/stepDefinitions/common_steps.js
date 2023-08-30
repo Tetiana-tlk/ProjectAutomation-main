36030030dcf5e554b42b858be060235d13a1c938
Tetiana <tatyanaleshchenkomodern@gmail.com>
2023-08-30 20:25:16 +0200

    30/08 test

--- cypress/stepDefinitions/common_steps.js
+++ cypress/stepDefinitions/common_steps.js
@@ -13,7 +13,7 @@ before(() => {
 });
 
 
-//dz+dz2+test 29/09+test 2_test3
+//dz+dz2+test 29/09+test 2_test3+test4
 When("I wait for {string} seconds", (waitingTime) => {
     switch (waitingTime) {
         case "2":
